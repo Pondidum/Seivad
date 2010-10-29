0371a777d02cad72ab7f8c0ea9e92c92a31c5b5f
Pondidum <Pondidum@stormbase.net>
2010-10-29 13:18:11 +0100

    added specs project, objectCreator

---
+++ Seivad/ObjectCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Seivad
+{
+    internal class ObjectCreator
+    {
+        public Type ReturnType { get; set; }
+        public bool IsSingleton { get; set; }
+
+        internal ObjectCreator()
+        {
+            ReturnType = null;
+            IsSingleton = false;
+        }
+
+        public Object GetInstance()
+        { 
+            
+        }
+    }
+}
