04564d0628ce5e4e6e325e22528ffbec101ae7b8
Pondidum <Pondidum@stormbase.net>
2010-10-31 14:42:34 +0000

    added Arguments and Argument classes

---
+++ Seivad/Arguments/Argument.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Seivad.Arguments
+{
+    public abstract class Argument
+    {
+        public string Name { get; private set; }
+        public Object Value { get; private set; }
+
+        public Argument(string name, Object value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
---
+++ Seivad/Arguments/Arguments.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Seivad.Arguments
+{
+    public abstract class Arguments
+    {
+        static Argument Add(string name, Object value)
+        {
+            return new Argument(name, value);
+        }
+    }
+}
