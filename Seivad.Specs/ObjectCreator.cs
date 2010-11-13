57be9fb9f60b2f2ff8747e232b863495634a61c1
Pondidum <Pondidum@stormbase.net>
2010-11-13 13:09:43 +0000

    more specs for objectCreator

--- Seivad.Specs/ObjectCreator.cs
+++ Seivad.Specs/ObjectCreator.cs
@@ -6,20 +6,67 @@ using Machine.Specifications;
 
 namespace Seivad.Specs
 {
-    class When_passed_a_type_with_no_public_constructor_and_no_arguments
+    public class When_passed_a_type_with_no_public_constructor_and_no_arguments
     {
         Establish context = () => objectCreator.ReturnType = typeof(NoPublicConstructor);
-
         Because of = () => ex = Catch.Exception(() => objectCreator.GetInstance());
 
-        It should_throw_a_constuctor_not_found_exception = () => ex.ShouldBeOfType<ConstructorNotFoundException>();
+        It should_throw_a_constuctor_exception = () =>
+        {
+            ex.ShouldNotBeNull();
+            ex.ShouldBeOfType<ConstructorNotFoundException>();
+        };
 
         static ObjectCreator objectCreator = new ObjectCreator();
         static Exception ex;
     }
 
+    public class When_passed_a_type_with_no_public_constructor_and_arguments
+    {
+        Establish context = () => objectCreator.ReturnType = typeof(NoPublicConstructor);
+        Because of = () => ex = Catch.Exception(() => objectCreator.GetInstance());
+
+        It should_throw_a_constructor_exception = () =>
+        {
+            ex.ShouldNotBeNull();
+            ex.ShouldBeOfType<ConstructorNotFoundException>();
+        };
+
+        static ObjectCreator objectCreator = new ObjectCreator();
+        static Exception ex;
+    }
+
+    public class When_passed_a_type_with_only_a_default_constructor_and_no_arguments
+    {
+        Establish context = () => objectCreator.ReturnType = typeof(DefaultConstructor);
+        Because of = () => obj = objectCreator.GetInstance();
+
+        It should_return_an_instance = () =>
+        {
+            obj.ShouldNotBeNull();
+            obj.ShouldBeOfType<DefaultConstructor>();
+        };
+
+        static ObjectCreator objectCreator = new ObjectCreator();
+        static Object obj;
+    }
+
+    public class When_passed_a_type_with_only_a_default_constructor_and_arguments
+    {
+        Establish context = () => objectCreator.ReturnType = typeof(DefaultConstructor);
+        Because of = () => ex = Catch.Exception(() => objectCreator.GetInstance());
+
+        It should_throw_a_constructor_exception = () =>
+        {
+            ex.ShouldNotBeNull();
+            ex.ShouldBeOfType<ConstructorNotFoundException>();
+        };
 
 
+
+        static ObjectCreator objectCreator = new ObjectCreator();
+        static Exception  ex;
+    }
 
 
 
@@ -27,6 +74,13 @@ namespace Seivad.Specs
     class NoPublicConstructor
     {
         private NoPublicConstructor()
+        {
+        }
+    }
+
+    class DefaultConstructor
+    {
+        public DefaultConstructor()
         {
         }
     }
