792ded9e775a9ccbcf833b6ca52606647b0f0c8d
Pondidum <Pondidum@stormbase.net>
2010-11-17 13:08:52 +0000

    moved more bits to the baseSpec

--- Seivad.Specs/ObjectCreatorSpecs.cs
+++ Seivad.Specs/ObjectCreatorSpecs.cs
@@ -9,6 +9,10 @@ namespace Seivad.Specs
 {
     public class NoRepositoryBase : SpecBase
     {
+
+        Because of = () => ex = Catch.Exception(() => obj = objectCreator.GetInstance(args));
+
+        static protected object obj;
         static protected IArguments args;
         static internal ObjectCreator objectCreator = new ObjectCreator();
     }
@@ -16,8 +20,12 @@ namespace Seivad.Specs
     [Subject("With no Repository")]
     public class When_passed_a_type_with_no_public_constructor_and_no_arguments : NoRepositoryBase
     {
-        Establish context = () => objectCreator.ReturnType = typeof(NoPublicConstructor);
-        Because of = () => ex = Catch.Exception(() => objectCreator.GetInstance(new ArgumentsDefinition()));
+        Establish context = () =>
+        {
+            objectCreator.ReturnType = typeof(NoPublicConstructor);
+            args = MockRepository.GenerateMock<IArguments>();
+            args.Expect(a => a.ToDictionary()).Return(new Dictionary<string, object>());
+        };
 
         It should_throw_a_constuctor_exception = () => ex.ShouldBeOfType<ConstructorException>();
     }
@@ -25,22 +33,27 @@ namespace Seivad.Specs
     [Subject("With no Repository")]
     public class When_passed_a_type_with_no_public_constructor_and_arguments : NoRepositoryBase
     {
-        Establish context = () => objectCreator.ReturnType = typeof(NoPublicConstructor);
-        Because of = () => ex = Catch.Exception(() => objectCreator.GetInstance(new ArgumentsDefinition()));
+        Establish context = () =>
+        {
+            objectCreator.ReturnType = typeof(NoPublicConstructor);
+            args = MockRepository.GenerateMock<IArguments>();
+            args.Expect(a => a.ToDictionary()).Return(new Dictionary<string, object>());
+        };
 
         It should_throw_a_constructor_exception = () => ex.ShouldBeOfType<ConstructorException>();
-
     }
 
     [Subject("With no Repository")]
     public class When_passed_a_type_with_only_a_default_constructor_and_no_arguments : NoRepositoryBase
     {
-        Establish context = () => objectCreator.ReturnType = typeof(DefaultConstructor);
-        Because of = () => obj = objectCreator.GetInstance(new ArgumentsDefinition());
+        Establish context = () =>
+        {
+            objectCreator.ReturnType = typeof(DefaultConstructor);
+            args = MockRepository.GenerateMock<IArguments>();
+            args.Expect(a => a.ToDictionary()).Return(new Dictionary<string, object>());
+        };
 
         It should_return_an_instance = () => obj.ShouldBeOfType<DefaultConstructor>();
-
-        static object obj;
     }
 
     [Subject("With no Repository")]
@@ -48,27 +61,25 @@ namespace Seivad.Specs
     {
         Establish context = () =>
         {
+            objectCreator.ReturnType = typeof(DefaultConstructor);
             args = MockRepository.GenerateMock<IArguments>();
             args.Expect(a => a.ToDictionary()).Return(new Dictionary<string, object>() { { "test", "value" } });
-
-            objectCreator.ReturnType = typeof(DefaultConstructor);
         };
 
-        Because of = () => ex = Catch.Exception(() => objectCreator.GetInstance(args));
-
         It should_throw_a_constructor_exception = () => ex.ShouldBeOfType<ConstructorException>();
-
     }
 
     [Subject("With no Repository")]
     public class When_passed_a_type_with_one_parameterised_constructor_and_no_arguments : NoRepositoryBase
     {
-        Establish context = () => objectCreator.ReturnType = typeof(ParameterisedOneArgument);
-
-        Because of = () => ex = Catch.Exception(() => objectCreator.GetInstance(new ArgumentsDefinition()));
+        Establish context = () =>
+        {
+            objectCreator.ReturnType = typeof(ParameterisedOneArgument);
+            args = MockRepository.GenerateMock<IArguments>();
+            args.Expect(a => a.ToDictionary()).Return(new Dictionary<string, object>());
+        };
 
         It should_throw_a_constructor_exception = () => ex.ShouldBeOfType<ConstructorException>();
-
     }
 
     [Subject("With no Repository")]
@@ -76,16 +87,12 @@ namespace Seivad.Specs
     {
         Establish context = () =>
         {
+            objectCreator.ReturnType = typeof(ParameterisedOneArgument);
             args = MockRepository.GenerateMock<IArguments>();
             args.Expect(a => a.ToDictionary()).Return(new Dictionary<string, object>() { { "test", "value" } });
-
-            objectCreator.ReturnType = typeof(ParameterisedOneArgument);
         };
 
-        Because of = () => ex = Catch.Exception(() => objectCreator.GetInstance(args));
-
         It should_throw_a_constructor_exception = () => ex.ShouldBeOfType<ConstructorException>();
-
     }
 
     [Subject("With no Repository")]
@@ -93,16 +100,12 @@ namespace Seivad.Specs
     {
         Establish context = () =>
         {
+            objectCreator.ReturnType = typeof(ParameterisedOneArgument);
             args = MockRepository.GenerateMock<IArguments>();
             args.Expect(a => a.ToDictionary()).Return(new Dictionary<string, object>() { { "argument", 10 } });
-
-            objectCreator.ReturnType = typeof(ParameterisedOneArgument);
         };
 
-        Because of = () => ex = Catch.Exception(() => objectCreator.GetInstance(args));
-
         It should_throw_a_constructor_exception = () => ex.ShouldBeOfType<ConstructorException>();
-
     }
 
     [Subject("With no Repository")]
@@ -110,17 +113,12 @@ namespace Seivad.Specs
     {
         Establish context = () =>
         {
+            objectCreator.ReturnType = typeof(ParameterisedOneArgument);
             args = MockRepository.GenerateMock<IArguments>();
             args.Expect(a => a.ToDictionary()).Return(new Dictionary<string, object>() { { "argument", "test" } });
-
-            objectCreator.ReturnType = typeof(ParameterisedOneArgument);
         };
 
-        Because of = () => obj = objectCreator.GetInstance(args);
-
         It should_return_an_instance = () => obj.ShouldBeOfType<ParameterisedOneArgument>();
-
-        static object obj;
     }
 
 
@@ -130,18 +128,13 @@ namespace Seivad.Specs
     {
         Establish context = () =>
         {
+            objectCreator.ReturnType = typeof(DefaultAndParameterisedOneArgument);
             args = MockRepository.GenerateMock<IArguments>();
             args.Expect(a => a.ToDictionary()).Return(new Dictionary<string, object>());
-
-            objectCreator.ReturnType = typeof(DefaultAndParameterisedOneArgument);
         };
-
-        Because of = () => obj = objectCreator.GetInstance(args);
 
         It should_return_an_instance = () => obj.ShouldNotBeNull();
         It should_call_the_default_constructor = () => ((DefaultAndParameterisedOneArgument)obj).ConstructorCalled.ShouldEqual("Default");
-        
-        static object obj;
     }
 
 
@@ -150,37 +143,28 @@ namespace Seivad.Specs
     {
         Establish context = () =>
         {
-            args = MockRepository.GenerateMock<IArguments>();
-            args.Expect(a => a.ToDictionary()).Return(new Dictionary<string, object>() { {"argument", "test" } });
-
             objectCreator.ReturnType = typeof(DefaultAndParameterisedOneArgument);
+            args = MockRepository.GenerateMock<IArguments>();
+            args.Expect(a => a.ToDictionary()).Return(new Dictionary<string, object>() { { "argument", "test" } });
         };
-
-        Because of = () => obj = objectCreator.GetInstance(args);
 
         It should_return_an_instance = () => obj.ShouldNotBeNull();
         It should_call_the_parameterised_constructor = () => ((DefaultAndParameterisedOneArgument)obj).ConstructorCalled.ShouldEqual("Parameterised");
-
-        static object obj;
     }
 
 
     [Subject("With no Repository")]
     public class When_passed_a_type_with_default_and_parameterised_constructors_and_one_non_matching_by_name_argument : NoRepositoryBase
-    { 
-         Establish context = () =>
+    {
+        Establish context = () =>
         {
-            args = MockRepository.GenerateMock<IArguments>();
-            args.Expect(a => a.ToDictionary()).Return(new Dictionary<string, object>() { {"TEST", "test" } });
-
-            objectCreator.ReturnType = typeof(DefaultAndParameterisedOneArgument);
+           objectCreator.ReturnType = typeof(DefaultAndParameterisedOneArgument);
+           args = MockRepository.GenerateMock<IArguments>();
+           args.Expect(a => a.ToDictionary()).Return(new Dictionary<string, object>() { { "TEST", "test" } });
         };
-        
-        Because of = () => ex = Catch.Exception(() => obj = objectCreator.GetInstance(args));
 
         It should_throw_a_constructor_exception = () => ex.ShouldBeOfType<ConstructorException>();
 
-        static object obj;
     }
 
 
@@ -190,18 +174,12 @@ namespace Seivad.Specs
 
         Establish context = () =>
         {
+            objectCreator.ReturnType = typeof(DefaultAndParameterisedOneArgument);
             args = MockRepository.GenerateMock<IArguments>();
             args.Expect(a => a.ToDictionary()).Return(new Dictionary<string, object>() { { "argument", 8 } });
-
-            objectCreator.ReturnType = typeof(DefaultAndParameterisedOneArgument);
         };
 
-        Because of = () => ex = Catch.Exception(() => obj = objectCreator.GetInstance(args));
-
         It should_throw_a_constructor_exception = () => ex.ShouldBeOfType<ConstructorException>();
-
-        static object obj;
-    
     }
 
 
@@ -231,15 +209,15 @@ namespace Seivad.Specs
 
     class DefaultAndParameterisedOneArgument
     {
-        public string ConstructorCalled {get; private set;}
+        public string ConstructorCalled { get; private set; }
 
         public DefaultAndParameterisedOneArgument()
-        { 
+        {
             ConstructorCalled = "Default";
         }
 
         public DefaultAndParameterisedOneArgument(string argument)
-        { 
+        {
             ConstructorCalled = "Parameterised";
         }
     }
