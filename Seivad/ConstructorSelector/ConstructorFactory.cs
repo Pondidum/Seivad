e274ebede9988d69348c92b53b22cd2e070fd09e
Pondidum <Pondidum@stormbase.net>
2010-11-19 13:11:50 +0000

    started on constructor matching

---
+++ Seivad/ConstructorSelector/ConstructorFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Seivad.ConstructorSelector
+{
+    class ConstructorFactory
+    {
+        private readonly static IList<IConstructorSelector> _handler;
+
+        static ConstructorFactory()
+        {
+            _handler = new List<IConstructorSelector>();
+
+            _handler.Add(new DefaultOnlySelector());
+        }
+
+        static IConstructorSelector GetConstructorHandler(IList<ConstructorInfo> constructors, IArguments arguments)
+        {
+            var handler = _handler.Where(h => h.CanHandle(constructors, arguments));
+
+            if (handler.Count() == 0)
+            {
+                throw new NotImplementedException("No ConstructorHandler for this type has been implemented");
+            }
+
+            return handler.First();
+        }
+    }
+}
---
+++ Seivad/ConstructorSelector/IConstructorSelector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Seivad.ConstructorSelector
+{
+    interface IConstructorSelector
+    {
+        bool CanHandle(IList<ConstructorInfo> constructors, IArguments arguments);
+        ConstructorInfo GetConstructor(IList<ConstructorInfo> constructors, IArguments arguments);
+    }
+}
